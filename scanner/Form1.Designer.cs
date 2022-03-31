97cfddf8b3a6ffdb5edcdab088727074ae75c464
Toshiyuki-Yamashita <Toshiyuki-Yamashita@users.noreply.github.com>
2022-03-31 17:25:32 +0900

    advの取得と表示

--- scanner/Form1.Designer.cs
+++ scanner/Form1.Designer.cs
@@ -28,19 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.logpath = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1303, 77);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.logpath);
+            this.flowLayoutPanel1.Controls.Add(this.log);
+            this.flowLayoutPanel1.Controls.Add(this.start);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1303, 77);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // logpath
+            // 
+            this.logpath.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.logpath.Location = new System.Drawing.Point(3, 0);
+            this.logpath.Name = "logpath";
+            this.logpath.Size = new System.Drawing.Size(800, 34);
+            this.logpath.TabIndex = 0;
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(809, 3);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(112, 34);
+            this.log.TabIndex = 1;
+            this.log.Text = "参照...";
+            this.log.UseVisualStyleBackColor = true;
+            this.log.Click += new System.EventHandler(this.log_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(927, 3);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(112, 34);
+            this.start.TabIndex = 2;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1303, 750);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1303, 827);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label logpath;
+        private Button log;
+        private Button start;
     }
 }
--- scanner/Form1.cs
+++ scanner/Form1.cs
@@ -1,10 +1,98 @@
+using Windows.Devices.Bluetooth.Advertisement;
+using System.Reflection;
 namespace scanner
 {
     public partial class Form1 : Form
     {
+        private BluetoothLEAdvertisementWatcher watcher;
+        private System.ComponentModel.BindingList<BluetoothLEAdvertisementReceivedEventArgs> db;
+        bool IsStarted
+        {
+            get
+            {
+                return watcher.Status == BluetoothLEAdvertisementWatcherStatus.Started;
+            }
+            set
+            {
+                if (value)
+                {
+                    start_watcher();
+                }
+                else
+                {
+                    stop_watcher();
+                }
+            }
+        }
+        
         public Form1()
         {
             InitializeComponent();
+            watcher = new BluetoothLEAdvertisementWatcher();
+            watcher.ScanningMode = BluetoothLEScanningMode.Passive;
+            watcher.Received += OnReceived;
+            db = new System.ComponentModel.BindingList<BluetoothLEAdvertisementReceivedEventArgs>();
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = db;
+            dataGridView1.GetType().InvokeMember("DoubleBuffered",
+                BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty,
+                null,
+                dataGridView1,
+                new object[] { true});
+
+        }
+
+        private async void OnReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs ev)
+        {
+            var data = await Task.Run(() => {
+                return ev;
+            });
+            add_data(data);
+        }
+
+        private void log_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                logpath.Text = dialog.SelectedPath;
+            }
+        }
+        private void add_data(BluetoothLEAdvertisementReceivedEventArgs data)
+        {
+            if (IsDisposed) return;
+            if (InvokeRequired)
+            {
+                Invoke((Action)(() => { add_data(data); }));
+            }
+            else
+            {
+                if (db.Count >= 100)
+                {
+                    db.RemoveAt(0);
+                }
+                db.Add(data);
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            IsStarted = !IsStarted;
+        }
+        private void stop_watcher()
+        {
+            if (!IsStarted) return;
+            watcher.Stop();
+        }
+        private void start_watcher()
+        {
+            if (IsStarted) return;
+            watcher.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IsStarted = false;
         }
     }
 }
